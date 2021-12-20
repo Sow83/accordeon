82a2ef8e8c10b2905538bfdec0ebc4482ee78d65
MOUHAMADOU  Sow <taftaf2512@gmail.com>
2021-12-20 10:08:13 +0100

    First commit

---
+++ main.js
@@ -0,0 +1,16 @@
+//Sélection des éléments(article)
+const questions = document.querySelectorAll(".question");
+//Ajout et suppression de classe dans les éléméents (article)
+questions.forEach(question => {
+    const questionBtn = question.querySelector(".question-btn");
+    questionBtn .addEventListener("click", function () {
+        questions.forEach(item => {
+            //Fait disparaitre le texte précédemment ouvert lors d'un clic  sur un autre bouton  
+            if (item !== question) {
+                item.classList.remove("show-text");         
+            }
+        });
+        //ajoute et enlève la classe show-text dans toutes les balises articles
+        question.classList.toggle("show-text"); 
+    }); 
+});
